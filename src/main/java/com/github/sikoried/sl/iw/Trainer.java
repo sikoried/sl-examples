23a5405cd200b4411a22b1445b7b374003b984d2
Korbinian Riedhammer <korbinianr@gmail.com>
2018-05-08 14:44:16 +0200

    Musterloesung iw

--- src/main/java/com/github/sikoried/sl/iw/Trainer.java
+++ src/main/java/com/github/sikoried/sl/iw/Trainer.java
@@ -1,24 +1,29 @@
 package com.github.sikoried.sl.iw;
 
+import com.github.sikoried.jstk.exceptions.AlignmentException;
+import com.github.sikoried.jstk.io.FrameInputStream;
+import com.github.sikoried.jstk.io.FrameSource;
+import com.github.sikoried.jstk.stat.*;
+import com.github.sikoried.jstk.stat.hmm.*;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Set;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Trainer {
 	private static Logger logger = LogManager.getLogger(Trainer.class);
-	public static void main(String[] args) throws ConfigurationException, IOException {
+	public static void main(String[] args) throws ConfigurationException, IOException, AlignmentException, ClassNotFoundException {
 		if (args.length != 1) {
 			logger.error("Usage: Trainer config.properties");
 			System.exit(0);
@@ -67,8 +72,175 @@ public class Trainer {
 
 		// TODO
 		// 0. for each class, allocate a HMM; use SCState if codebook is specified, CState otherwise
+
+		// codebook laden, sofern vorhanden
+		Mixture shared = (codebook == null
+			? null
+			: new Mixture(new FileInputStream(codebook)));
+
+		// template state erstellen
+		State templ = (codebook == null
+				? new CState(new Mixture(24, num_densities, true))
+				: new SCState(shared));
+
+		// HMMs anlegen
+		Map<String, Hmm> models = new TreeMap<>();
+		for (String c : classes) {
+			Hmm hmm = new Hmm(Integer.parseInt(c), (short) num_states, templ);
+
+			// Topologie waehlen
+			hmm.setTransitions(Hmm.Topology.LINEAR);
+
+			models.put(c, hmm);
+		}
+
+
+		// cache dataset
+		List<Pair<String, List<double[]>>> data = new LinkedList<>();
+		BufferedReader br = new BufferedReader(new FileReader(listf));
+		String line;
+		while ((line = br.readLine()) != null) {
+			String cl = line.split("_")[0];
+
+			if (!models.containsKey(cl)) {
+				logger.warn("Ignoring file with invalid class " + cl);
+				continue;
+			}
+
+			FrameSource fs = new FrameInputStream(new File(ft_dir, line));
+			List<double[]> samples = new LinkedList<>();
+			double[] buf = new double [fs.getFrameSize()];
+			while (fs.read(buf))
+				samples.add(buf.clone());
+
+			data.add(Pair.of(cl, samples));
+		}
+
+
 		// 1. compute an initial estimate, by creating linear alignments for each file and class; save
 		//    the initial estimates as <name>.mdl.0
+
+		// initialize the models
+		MleMixtureAccumulator shared_accumulator = (shared == null
+			? null
+			: new MleMixtureAccumulator(24, shared.nd, DensityDiagonal.class));
+
+		for (Hmm hmm : models.values()) {
+			hmm.init();
+
+			// must set the shared accumulator (a bit of a pain)
+			if (shared != null) {
+				for (State s : hmm.s)
+					if (s instanceof SCState)
+						((SCState) s).setSharedAccumulator(shared_accumulator);
+			}
+		}
+
+		// compute forced-linear
+		List<Alignment> cached_alignments = new LinkedList<>();
+		for (Pair<String, List<double[]>> p : data) {
+			Hmm target = models.get(p.getKey());
+			Alignment ali = new Alignment(target, p.getValue());
+			ali.forceLinearAlignment();
+			target.incrementVT(ali);
+
+			// cache
+			cached_alignments.add(ali);
+		}
+
+		// pain...
+		if (shared_accumulator != null) {
+			Mixture old = shared.clone();
+			MleMixtureAccumulator.MleUpdate(old,
+					MleDensityAccumulator.MleOptions.pDefaultOptions,
+					Density.Flags.fAllParams,
+					shared_accumulator,
+					shared);
+
+			shared_accumulator.flush();
+
+			shared.write(new FileOutputStream(new File(mdl_dir, "codebook.mdl.0")));
+		}
+
+		for (Map.Entry<String, Hmm> p : models.entrySet()) {
+			Hmm hmm = p.getValue();
+
+			hmm.reestimate();
+			hmm.discard();
+
+			hmm.write(new FileOutputStream(new File(mdl_dir, p.getKey() + ".mdl.0")));
+		}
+
+
+		for (int i = 1; i <= num_iters; i++) {
+			for (Hmm hmm : models.values())
+				hmm.init();
+
+			if (realign.contains(i)) {
+				logger.info("re-aligning data...");
+
+				// discard old ali
+				cached_alignments.clear();
+
+				double avg = 0.0;
+				for (Pair<String, List<double[]>> p : data) {
+					Hmm target = models.get(p.getKey());
+					Alignment ali = new Alignment(target, p.getValue());
+					double obj = ali.forcedAlignment();
+
+					// cache
+					cached_alignments.add(ali);
+
+					// debug
+					avg += obj;
+					logger.info("cl= " + p.getKey() + " obj=" + obj);
+				}
+
+				logger.info("average obj=" + (avg / cached_alignments.size()));
+			}
+
+			Iterator<Pair<String, List<double[]>>> it1 = data.iterator();
+			Iterator<Alignment> it2 = cached_alignments.iterator();
+
+			while (it1.hasNext()) {
+				Pair<String, List<double[]>> p = it1.next();
+				Alignment ali = it2.next();
+
+				models.get(p.getKey()).incrementVT(ali);
+			}
+
+			for (Map.Entry<String, Hmm> p : models.entrySet()) {
+				Hmm hmm = p.getValue();
+
+				hmm.reestimate();
+				hmm.discard();
+
+				hmm.write(new FileOutputStream(new File(mdl_dir, p.getKey() + ".mdl." + i)));
+			}
+
+			// pain...
+			if (shared_accumulator != null) {
+				Mixture old = shared.clone();
+				MleMixtureAccumulator.MleUpdate(old,
+						MleDensityAccumulator.MleOptions.pDefaultOptions,
+						Density.Flags.fAllParams,
+						shared_accumulator,
+						shared);
+
+				shared_accumulator.flush();
+
+				shared.write(new FileOutputStream(new File(mdl_dir, "codebook.mdl." + i)));
+			}
+		}
+
+		if (codebook != null)
+			Files.createSymbolicLink((new File(mdl_dir, "codebook.mdl")).toPath(),
+					(new File("codebook.mdl." + num_iters)).toPath());
+
+		for (String cl : classes)
+			Files.createSymbolicLink((new File(mdl_dir, cl + ".mdl")).toPath(),
+					(new File(cl + ".mdl." + num_iters)).toPath());
+
 		// 2. reset the accumulators
 		// 3. if (cur_iter in realign): compute the forced alignment
 		// 4. accumulate according to alignments (eg. `accumulateVT`)
