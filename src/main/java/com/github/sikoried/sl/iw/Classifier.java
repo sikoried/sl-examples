f9768675e4d053d623e822d0f2918f1e48ccd57d
Korbinian Riedhammer <korbinianr@gmail.com>
2020-03-26 07:34:15 +0100

    IW solution new :

--- src/main/java/com/github/sikoried/sl/iw/Classifier.java
+++ src/main/java/com/github/sikoried/sl/iw/Classifier.java
@@ -1,17 +1,26 @@
 package com.github.sikoried.sl.iw;
 
+import com.github.sikoried.jstk.arch.Codebook;
+import com.github.sikoried.jstk.exceptions.AlignmentException;
+import com.github.sikoried.jstk.io.FrameInputStream;
+import com.github.sikoried.jstk.io.FrameSource;
+import com.github.sikoried.jstk.stat.hmm.Alignment;
+import com.github.sikoried.jstk.stat.hmm.Hmm;
+import com.github.sikoried.jstk.util.Arithmetics;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
+import java.io.*;
+import java.util.*;
 
 public class Classifier {
 	private static Logger logger = LogManager.getLogger(Classifier.class);
 
-	public static void main(String[] args) throws ConfigurationException {
+	public static void main(String[] args) throws ConfigurationException, FileNotFoundException, IOException {
 		if (args.length != 1) {
 			logger.error("Usage: Trainer config.properties");
 			System.exit(0);
@@ -35,6 +44,48 @@ public class Classifier {
 
 		// TODO
 		// 1. Load the model files (and codebook)
+		Map<String, Hmm> mdls = new HashMap<>();
+
+		for (String cl : classes) {
+			Hmm hmm = new Hmm(new FileInputStream(new File(mdl_dir, cl + ".mdl.20")), null);
+			mdls.put(cl, hmm);
+		}
+
+		BufferedReader br = new BufferedReader(new FileReader(new File(listf)));
+		br.lines().forEach(line -> {
+			try {
+				String[] p = line.split("_");
+				String correct = p[0];
+
+				FrameSource fs = new FrameInputStream(new File(ft_dir, line));
+
+				List<double[]> xs = new LinkedList<>();
+				double[] buf = new double [fs.getFrameSize()];
+				while (fs.read(buf))
+					xs.add(buf.clone());
+
+				double[] scores = new double [classes.length];
+				String[] names = new String[classes.length];
+				int i = 0;
+				for (Map.Entry<String, Hmm> e : mdls.entrySet()) {
+					Alignment ali = new Alignment(e.getValue(), xs);
+					scores[i] = ali.forcedAlignment();
+					names[i++] = e.getKey();
+				}
+
+				Arithmetics.makesumto1(scores);
+				int max = Arithmetics.maxi(scores);
+
+				logger.info(line + " " + names[max] + " " + Arrays.toString(scores)
+						+ " " + Arrays.toString(names));
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (AlignmentException e) {
+				e.printStackTrace();
+			}
+		});
+
 		// 2. For each feature file, align each of the models
 		// 3. Normalize the scores to a soft-max (to get probabilities for each class)
 		// 4. Output lines of "<file> <best-class> <class-scores ...>"
--- src/main/java/com/github/sikoried/sl/iw/Trainer.java
+++ src/main/java/com/github/sikoried/sl/iw/Trainer.java
@@ -1,19 +1,25 @@
 package com.github.sikoried.sl.iw;
 
+import com.github.sikoried.jstk.exceptions.AlignmentException;
+import com.github.sikoried.jstk.io.FrameInputStream;
+import com.github.sikoried.jstk.io.FrameReader;
+import com.github.sikoried.jstk.io.FrameSource;
+import com.github.sikoried.jstk.stat.Mixture;
+import com.github.sikoried.jstk.stat.hmm.Alignment;
+import com.github.sikoried.jstk.stat.hmm.CState;
+import com.github.sikoried.jstk.stat.hmm.Hmm;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Trainer {
@@ -53,7 +59,7 @@ public class Trainer {
 		String codebook = config.getString("iw.codebook", null);
 
 		// CState: number of densities per-state
-		int num_densities = config.getInt("iw.densities", 1);
+		int num_densities = config.getInt("iw.densities", 3);
 
 		logger.info("classes = " + Arrays.toString(classes));
 		logger.info("list = " + listf);
@@ -67,12 +73,99 @@ public class Trainer {
 
 		// TODO
 		// 0. for each class, allocate a HMM; use SCState if codebook is specified, CState otherwise
+		Map<String, Hmm> mdls = new HashMap<>();
+		Mixture mix = new Mixture(24, num_densities, true);
+		for (String c : classes) {
+			Hmm hmm = new Hmm(Integer.parseInt(c), (short) num_states, new CState(mix));
+			hmm.setTransitions(Hmm.Topology.LINEAR);
+			mdls.put(c, hmm);
+		}
+
+
 		// 1. compute an initial estimate, by creating linear alignments for each file and class; save
 		//    the initial estimates as <name>.mdl.0
-		// 2. reset the accumulators
-		// 3. if (cur_iter in realign): compute the forced alignment
-		// 4. accumulate according to alignments (eg. `accumulateVT`)
-		// 5. re-estimate the parameters (`.reestimate()`), save current estimate
-		// 6. if (cur_iter < num_iters): goto 2
+		mdls.values().forEach(Hmm::init);
+
+		List<Pair<String, List<double[]>>> samples = new LinkedList<>();
+		BufferedReader br = new BufferedReader(new FileReader(new File(listf)));
+		br.lines().forEach(line -> {
+			try {
+				String[] p = line.split("_");
+				String c = p[0];
+
+				FrameSource fs = new FrameInputStream(new File(ft_dir, line));
+
+				List<double[]> xs = new LinkedList<>();
+				double[] buf = new double [fs.getFrameSize()];
+				while (fs.read(buf))
+					xs.add(buf.clone());
+
+				samples.add(Pair.of(c, xs));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
+		samples.forEach(p -> {
+			try {
+				Hmm hmm = mdls.get(p.getKey());
+				Alignment ali = new Alignment(hmm, p.getValue());
+				ali.forceLinearAlignment();
+				hmm.incrementVT(ali);
+			} catch (AlignmentException e) {
+				e.printStackTrace();
+			}
+		});
+
+		mdls.values().forEach(Hmm::reestimate);
+		mdls.values().forEach(Hmm::discard);
+
+		// write models to disk.
+		for (Map.Entry<String, Hmm> e : mdls.entrySet()) {
+			FileOutputStream fos = new FileOutputStream(new File(mdl_dir, e.getKey() + ".mdl.0"));
+			e.getValue().write(fos);
+			fos.close();
+		}
+
+		List<Pair<String, Alignment>> alis = new LinkedList<>();
+		for (int it = 1; it <= num_iters; ++it) {
+			logger.info("Iteration " + it);
+			mdls.values().forEach(Hmm::init);
+
+			if (realign.contains(it)) {
+				logger.info("Re-aligning");
+				alis.clear();
+				samples.forEach(p -> {
+					try {
+						Hmm hmm = mdls.get(p.getKey());
+						Alignment ali = new Alignment(hmm, p.getValue());
+
+						ali.forcedAlignment();
+						alis.add(Pair.of(p.getKey(), ali));
+					} catch (AlignmentException e) {
+						e.printStackTrace();
+					}
+				});
+			}
+
+			logger.info("Accumulating");
+			alis.forEach(a -> {
+				String cl = a.getKey();
+				Alignment ali = a.getValue();
+				mdls.get(cl).incrementVT(ali);
+			});
+
+			logger.info("Re-estimating");
+			mdls.values().forEach(Hmm::reestimate);
+			mdls.values().forEach(Hmm::discard);
+
+			for (Map.Entry<String, Hmm> e : mdls.entrySet()) {
+				FileOutputStream fos = new FileOutputStream(new File(mdl_dir, e.getKey() + ".mdl." + it));
+				e.getValue().write(fos);
+				fos.close();
+			}
+
+		}
+
 	}
 }
